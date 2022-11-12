8c25f68ae006a7c1dfa61785a5856cd39f8e5303
Prabin Karki <prabin.innovative@gmail.com>
2022-11-12 19:38:06 +0545

    completed UI

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomePage from "./src/screens/HomePage";
-import { CurrentWeatherProvider } from "./src/GetCurrentWeather/GetCurrentWeather";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CurrentWeatherProvider>
         <HomePage />
-      </CurrentWeatherProvider>
     </View>
   );
 }
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,139 +1,208 @@
-import React, { useRef, useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   StyleSheet,
   StatusBar,
+  Dimensions,
+  Image,
   ScrollView,
-  TouchableOpacity,
+  SafeAreaView,
   Alert,
+  ActivityIndicator,
+  RefreshControl,
 } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import LottieView from "lottie-react-native";
+import Feather from "react-native-vector-icons/Feather";
 import * as Location from "expo-location";
-import { GetCurrentWeather } from "../GetCurrentWeather/GetCurrentWeather";
 
-const data = [
-  {
-    time: "08:00pm",
-    icon: "sunny",
-    title: "clear",
-    temp: "25°C",
-  },
-  {
-    time: "08:00pm",
-    icon: "sunny",
-    title: "clear",
-    temp: "25°C",
-  },
-  {
-    time: "08:00pm",
-    icon: "sunny",
-    title: "clear",
-    temp: "25°C",
-  },
-  {
-    time: "08:00pm",
-    icon: "sunny",
-    title: "clear",
-    temp: "25°C",
-  },
-  {
-    time: "08:00pm",
-    icon: "sunny",
-    title: "clear",
-    temp: "25°C",
-  },
-  {
-    time: "08:00pm",
-    icon: "sunny",
-    title: "clear",
-    temp: "25°C",
-  },
-];
-
+const openWeatherKey = "012ec2c115e1e269eea0c935bcd04d79";
 
 function HomePage() {
-  const animation = useRef(null);
+  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [currentWeatherData, setCurrentWeatherData] = useState(null);
+  const [weatherCondition, setWeatherCondition] = useState(null);
+  const [sunSetTime, setSunSetTime] = useState(null);
+  const [sunRiseTime, setSunRiseTime] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [refreshPage, setRefreshPage] = useState(false)
 
-  //temperature from GetCurrentWeather
-  let { temperature, description, cityDetails } = useContext(GetCurrentWeather);
+  //get today date javascript
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
+  const weekday = new Array(
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  );
+  var dayOfWeek = weekday[today.getDay()];
+  const todayDate = dayOfWeek + ", " + mm + "/" + dd + "/" + yyyy;
 
+  useEffect(() => {
+    currentWeather();
+  }, []);
+
+  //get user's current location and fetch todays weather forecast
+  const currentWeather = async () => {
+    setRefreshPage(true)
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    //set current location
+    setLatitude(location.coords.latitude);
+    setLongitude(location.coords.longitude);
+
+    setLoading(true);
+    if (longitude && latitude) {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openWeatherKey}`
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        Alert.alert("Error", "Something went wrong", [
+          { text: "OK", onPress: () => console.warn("OK Pressed") },
+        ]);
+      } else {
+        setCurrentWeatherData(data);
+        setWeatherCondition(data.weather[0]["description"]);
+        const sunRise = new Date(data.sys.sunrise * 1000);
+        const sunSet = new Date(data.sys.sunset * 1000);
+        setSunRiseTime(sunRise);
+        setSunSetTime(sunSet);
+      }
+    }
+    setLoading(false);
+    setRefreshPage(false)
+  };
+
+  if (errorMsg) {
+    Alert.alert("Permission", errorMsg, [
+      { text: "OK", onPress: () => console.warn("OK Pressed") },
+    ]);
+  }
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <View style={{ alignItems: "center" }}>
-        <Text style={styles.location}>
-          {!cityDetails ? "Loading" : cityDetails[0]["city"]}
-        </Text>
-        <Ionicons name="location-outline" size={18} color="white" />
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-          paddingTop: 40,
-          marginVertical: 20,
-        }}
-      >
-        <LottieView
-          autoPlay
-          ref={animation}
+    <SafeAreaView style={styles.container}>
+      <ScrollView refreshControl={
+        <RefreshControl refreshing={refreshPage} onRefresh={()=> currentWeather()} />
+        
+      }>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.location}>
+            {currentWeatherData ? currentWeatherData.name : (<ActivityIndicator size="small" />)}
+          </Text>
+          <Ionicons name="location-outline" size={18} color="black" />
+        </View>
+        <View style={styles.tempContainer}>
+          {currentWeatherData ? (
+            <Image
+              source={{
+                uri: `http://openweathermap.org/img/w/${currentWeatherData.weather[0]["icon"]}.png`,
+              }}
+              style={{ height: 200, width: 200 }}
+            />
+          ) : (
+            <ActivityIndicator size="small" />
+          )}
+          <Text style={{ color: "black", fontSize: 30, fontWeight: "bold" }}>
+            {currentWeatherData
+              ? parseInt(currentWeatherData.main.temp - 273.15)
+              : (<ActivityIndicator size="small" />)}
+            °C
+          </Text>
+        </View>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ color: "black", fontSize: 24, fontWeight: "bold" }}>
+            {weatherCondition
+              ? weatherCondition
+                  .toLowerCase()
+                  .replace(/\b(\w)/g, (s) => s.toUpperCase())
+              : (<ActivityIndicator size="small" />)}
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 40, paddingLeft: 20 }}>
+          <Text style={{ fontWeight: "700", fontSize: 17 }}>{todayDate}</Text>
+        </View>
+
+        <View
           style={{
-            width: 150,
-            height: 200,
-          }}
-          // Find more Lottie files at https://lottiefiles.com/featured
-          source={require("../../assets/rainy.json")}
-        />
-        <Text style={{ color: "white", fontSize: 30 }}>{temperature}°C</Text>
-      </View>
-      <View style={{ justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ color: "white", fontSize: 24 }}>
-          {!description ? null : description.toLowerCase().replace(/\b(\w)/g, (s) => s.toUpperCase())}
-        </Text>
-      </View>
-      <View style={{ marginTop: 40 }}>
-        <Text style={{ color: "white", fontSize: 16, marginLeft: 10 }}>
-          Tuesday 11/8/2022
-        </Text>
-        <ScrollView horizontal={true}>
-          {data.map((data, index) => {
-            return (
-              <View
-                key={index}
-                style={{
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  marginHorizontal: 10,
-                  marginTop: 20,
-                }}
-              >
-                <Text>{data.time}</Text>
-                <Ionicons name={data.icon} size={25} color="white" />
-                <Text>{data.title}</Text>
-                <Text>{data.temp}</Text>
-              </View>
-            );
-          })}
-        </ScrollView>
-      </View>
-      <TouchableOpacity style={{ marginVertical: 50 }} onPress={() => {}}>
-        <Text
-          style={{
-            borderBottomColor: "white",
-            borderBottomWidth: 2,
-            color: "blue",
-            fontSize: 20,
-            alignSelf: "center",
+            marginTop: 10,
+            flexDirection: "row",
+            justifyContent: "space-around",
           }}
         >
-          7 Days Weather Forecast
-        </Text>
-      </TouchableOpacity>
-    </View>
+          <View style={styles.info}>
+            <Feather name="sunrise" size={25} color="#E67451" />
+            <Text style={styles.infoText}>
+              {currentWeatherData
+                ? sunRiseTime.toLocaleTimeString().replace(/:\+ /, "")
+                : (<ActivityIndicator size="small" />)}
+            </Text>
+            <Text style={styles.infoText}>Sun Rise</Text>
+          </View>
+          <View style={styles.info}>
+            <Feather name="sunset" size={25} color="#ffc922" />
+            <Text style={styles.infoText}>
+              {currentWeatherData
+                ? sunSetTime.toLocaleTimeString().replace(/:\+ /, "")
+                : (<ActivityIndicator size="small" />)}
+            </Text>
+            <Text style={styles.infoText}>Sun Set</Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            marginTop: 40,
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <View style={styles.info}>
+            <Ionicons name="thermometer" size={25} color="black" />
+            <Text style={styles.infoText}>
+              {currentWeatherData
+                ? parseInt(currentWeatherData.main.feels_like - 273.15)
+                : (<ActivityIndicator size="small" />)}
+              °C
+            </Text>
+            <Text style={styles.infoText}>Feels Like</Text>
+          </View>
+          <View style={styles.info}>
+            <Ionicons name="water-sharp" size={25} color="#87CEEB" />
+            <Text style={styles.infoText}>
+              {currentWeatherData
+                ? currentWeatherData.main.humidity
+                : (<ActivityIndicator size="small" />)}
+              %
+            </Text>
+            <Text style={styles.infoText}>Humidity</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -142,12 +211,27 @@ export default HomePage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "green",
   },
   location: {
     fontSize: 20,
     fontWeight: "bold",
-    color: "white",
+    color: "black",
     marginTop: StatusBar.currentHeight + 10,
   },
+  tempContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingTop: 40,
+    marginVertical: 20,
+  },
+  info: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    width: Dimensions.get("screen").width / 2.5,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 10,
+  },
+  infoText: { color: "white", fontSize: 18 },
 });
